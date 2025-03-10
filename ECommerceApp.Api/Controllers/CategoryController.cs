5de8e5f6afbe9e22c1434ea824da9f123962a46e
MykeLora <MykeLora@gmail.com>
2025-03-09 22:47:08 -0400

    Not ready at the moment

--- ECommerceApp.Api/Controllers/CategoryController.cs
+++ ECommerceApp.Api/Controllers/CategoryController.cs
@@ -1,31 +1,31 @@
-﻿using E_commerce.Domain.Entities.Products;
-using ECommerceApp.Application.Contracts;
-using ECommerceApp.DTOs.CategoryDTOs;
-using ECommerceApp.Persistence.Interfaces.Products;
-using Microsoft.AspNetCore.Mvc;
+﻿//using E_commerce.Domain.Entities.Products;
+//using ECommerceApp.Application.Contracts;
+//using ECommerceApp.DTOs.CategoryDTOs;
+//using ECommerceApp.Persistence.Interfaces.Products;
+//using Microsoft.AspNetCore.Mvc;
 
-namespace ECommerceApp.Api.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class CategoryController : ControllerBase
-    {
-        private readonly ICategoryService _categoryService;
+//namespace ECommerceApp.Api.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class CategoryController : ControllerBase
+//    {
+//        private readonly ICategoryService _categoryService;
 
-        public CategoryController(ICategoryService categoryService)
-        {
-            _categoryService = categoryService;
-        }
+//        public CategoryController(ICategoryService categoryService)
+//        {
+//            _categoryService = categoryService;
+//        }
 
-        [HttpGet("Get-All-Categories")]
-        public async Task<IActionResult> GetAllCategories()
-        {
-            var categories = await _categoryService.GetAll();
-            return Ok(categories);
-        }
+//        [HttpGet("Get-All-Categories")]
+//        public async Task<IActionResult> GetAllCategories()
+//        {
+//            var categories = await _categoryService.GetAll();
+//            return Ok(categories);
+//        }
 
         
 
 
-    }
-}
+//    }
+//}
