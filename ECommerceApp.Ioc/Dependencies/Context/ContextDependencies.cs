16ac0be785ff746f40770424e2f49ef7e94fb6cd
MykeLora <MykeLora@gmail.com>
2025-03-12 15:20:56 -0400

    Inversion of control implement and used in api's layer

---
+++ ECommerceApp.Ioc/Dependencies/Context/ContextDependencies.cs
@@ -0,0 +1,25 @@
+﻿using ECommerceApp.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApp.Ioc.Dependencies.Context
+{
+    public static class ContextDependencies
+    {
+
+        public static void AddContextDependencies(this IServiceCollection services, string connectionString )
+        {
+            services.AddDbContext<ApplicationContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+
+        }
+    }
+}
---
+++ ECommerceApp.Ioc/Dependencies/Product/ProductDependency.cs
@@ -0,0 +1,32 @@
+﻿using ECommerceApp.Application.Contracts;
+using ECommerceApp.Application.Services.Products;
+using ECommerceApp.Persistence.Interfaces.Products;
+using ECommerceApp.Persistence.Repositories.Products;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApp.Ioc.Dependencies.Product
+{
+    public static class ProductDependency
+    {
+        public static void AddProductDependencies(this IServiceCollection services)
+        {
+
+            // AddProductDependencies
+
+            #region "Repositories"
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            #endregion
+
+            #region "Services"
+            services.AddTransient<IProductService, ProductService>();
+            services.AddTransient<ICategoryService, CategoryService>();
+            #endregion
+        }
+    }
+}
